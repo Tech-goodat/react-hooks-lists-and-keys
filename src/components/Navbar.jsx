93a5db582e6e4f370f746327de52cdcd15c1900f
Tech-goodat <felixkiprotich2000@gmail.com>
2023-11-03 08:53:04 +0300

    done with the lab

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function NavBar() {
+  const links = ["home", "about", "projects"];
+
+  return (
+    <nav>
+      {links.map((link) => (
+        <a href={`#${link}`} key={link}>
+          {link}
+        </a>
+      ))}
+    </nav>
+  );
+}
+
+export default NavBar;
